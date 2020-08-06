ee20a7c2d8949213217029337d771e6aff7269e9
Patricia306 <69148030+Patricia306@users.noreply.github.com>
2020-08-06 19:49:38 +0200

    Update 3.1_DML_GrupoX.sql

--- 3 DML/3.1_DML_GrupoX.sql
+++ 3 DML/3.1_DML_GrupoX.sql
@@ -7,6 +7,16 @@ en el departamento de IT Innovation
 que se encuentra en la ciudad de Madrid, Spain
 Y como tipo de trabajo: Software Engineering 
 */
+insert into countries(country_ID, country_name)
+values ('SP', 'Spain');
+insert into locations(location_id, city)
+values ( 3300, 'Madrid');
+insert into departments(department_name, department_id)
+values('IT Innovation', 280);
+insert into jobs(job_id, job_title)
+values('SO_ENG', 'Software Engineering');
+insert into employees(employee_id, first_name, last_name, email, hire_date, job_id, department_id)
+values(207, 'James', 'Dexter', 'JDEXTER', '06/08/2020','SO_ENG', 280);
 /*
 Comprobar que se ven los datos insertados de forma conjunta con una JOIN
 y no de forma independiente. Con el fin de comprobar las relaciones.
