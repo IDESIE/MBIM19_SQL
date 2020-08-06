d17607722ef937879a8f203bc43c612a8f4bf0e2
ASA-ID <69035681+ASA-ID@users.noreply.github.com>
2020-08-06 20:12:54 +0200

    Update 3.1_DML_GrupoA1.sql

--- 3 DML/3.1_DML_GrupoA1.sql
+++ 3 DML/3.1_DML_GrupoA1.sql
@@ -28,15 +28,33 @@ values (207,'James','Dexter','JDEXTER','06/08/2020',
 Comprobar que se ven los datos insertados de forma conjunta con una JOIN
 y no de forma independiente. Con el fin de comprobar las relaciones.
 */
+select *
+from 
+    employees
+    join departments 
+        on employees.department_id = departments.department_id
+    join locations
+        on departments.location_id = locations.location_id
+    join countries
+        on locations.country_id = countries.country_id
+    join jobs
+        on employees.job_id = employees.job_id
+where employee_id=207 and jobs.job_id='IT_SOFT';
 
 /* 2
 Actualizarle el salario a 60000
 */
+update employees
+set salary = 6000
+where employee_id = 207;
 
 /* 3
 Colocarle una comisión igual a la media de comisiones
 Manteniendo dos centésimas como valor.
 */
+update employees
+set commission_pct = (select avg(commission_pct) from employees)
+where employee_id = 207;
 
 /* 4
 Anonimizar sus datos personales: nombre, apellido, email, teléfono
