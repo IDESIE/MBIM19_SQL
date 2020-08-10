4ef718f1348893f08ffad249480fb343ce9b1bb3
ASA-ID <69035681+ASA-ID@users.noreply.github.com>
2020-08-10 18:51:51 +0200

    Update 4.1_DDL_GrupoA1.sql

--- 4 DDL/4.1_DDL_GrupoA1.sql
+++ 4 DDL/4.1_DDL_GrupoA1.sql
@@ -17,6 +17,57 @@ description
 category
 address
 
+FLOORS
+id
+guid
+name
+category
+description
+height
+facilityId
+
+SPACES
+id
+guid
+name
+category
+description
+usableHeight
+area
+floorId
+
+COMPONENTS
+id
+guid
+name
+description
+serialNumber
+installatedOn
+spaceId
+typeId
+
+TYPES
+id
+guid
+name
+description
+modelNumber
+color
+warrantyYears
+
+En las definiciones establacer las siguientes restricciones
+-Los guid deben ser únicos.
+-No es posible dar de alta un componente sin un tipo.
+-No es posible dar de alta un espacio sin una planta.
+-No es posiblde dar de alta una planta sin un facility.
+-Dos componentes no pueden llamarse igual, lo mismo pasa con el resto de entidades.
+-La fecha de instalación de un componente por defecto es la actual.
+-Los nombres no pueden estar vacíos en todas las entidades.
+-Los años de garantia no pueden ser cero ni negativos.
+-Se debe mantener la integridad referencial.
+
+NOTA: Algunos ejercicios provocan errores que deben probar (para ver el error) y corregir.
+*/
 create table FACILITIES(
     id number,
     guid varchar2(4000),
@@ -27,15 +78,6 @@ create table FACILITIES(
     constraint pk_facilities_id primary key (id),
     constraint uq_facilities_guid unique(guid)
 );
-
-FLOORS
-id
-guid
-name
-category
-description
-height
-facilityId
 
 create table FLOORS(
     id number,
@@ -49,16 +91,6 @@ create table FLOORS(
     constraint uq_floors_guid unique(guid),
     constraint uq_floors_name unique(name)
 );
-
-SPACES
-id
-guid
-name
-category
-description
-usableHeight
-area
-floorId
 
 create table SPACES(
     id number,
@@ -74,15 +106,19 @@ create table SPACES(
     constraint uq_spaces_name unique(name)
 );
 
-COMPONENTS
-id
-guid
-name
-description
-serialNumber
-installatedOn
-spaceId
-typeId
+create table COMPONENTS(
+    id number,
+    guid varchar2(4000),
+    name varchar2(4000)  not null,
+    description varchar2 (4000),
+    serialNumber varchar2 (4000),
+    installatedOn date default sysdate,
+    spaceId number not null,
+    typeId number not null,
+    constraint pk_components_id primary key (id),
+    constraint uq_components_guid unique(guid),
+    constraint uq_components_name unique(name)
+);
 
 create table TYPES(
     id number,
@@ -98,39 +134,26 @@ create table TYPES(
     constraint ck_types_warrantyYears check(warrantyYears>0)
 );
 
-TYPES
-id
-guid
-name
-description
-modelNumber
-color
-warrantyYears
-
-create table COMPONENTS(
-    id number,
-    guid varchar2(4000),
-    name varchar2(4000)  not null,
-    description varchar2 (4000),
-    serialNumber varchar2 (4000),
-    installatedOn date default sysdate,
-    spaceId number not null,
-    typeId number not null,
-    constraint pk_components_id primary key (id),
-    constraint uq_components_guid unique(guid),
-    constraint uq_components_name unique(name)
+alter table FLOORS ADD (
+    constraint fk_floors_facilityId
+    foreign key (facilityId)
+    references FACILITIES (id)
 );
 
-En las definiciones establacer las siguientes restricciones
--Los guid deben ser únicos.
--No es posible dar de alta un componente sin un tipo.
--No es posible dar de alta un espacio sin una planta.
--No es posiblde dar de alta una planta sin un facility.
--Dos componentes no pueden llamarse igual, lo mismo pasa con el resto de entidades.
--La fecha de instalación de un componente por defecto es la actual.
--Los nombres no pueden estar vacíos en todas las entidades.
--Los años de garantia no pueden ser cero ni negativos.
--Se debe mantener la integridad referencial.
+alter table SPACES ADD (
+    constraint fk_spaces_floorId
+    foreign key (floorId)
+    references FLOORS (id)
+);
 
-NOTA: Algunos ejercicios provocan errores que deben probar (para ver el error) y corregir.
-*/
+alter table COMPONENTS ADD (
+    constraint fk_components_typeId
+    foreign key (typeId)
+    references TYPES (id)
+);
+
+alter table COMPONENTS ADD (
+    constraint fk_components_spaceId
+    foreign key (spaceId)
+    references SPACES (id)
+);
